a5c1436e70ea4e86ceac8fd1d90dcffa3e52deef
Francesco Borrelli <francesco.borrelli@digitarca.it>
2023-06-16 16:45:29 +0200

    Fix Interfaccia Modali

--- SmartCity3Dar/app/src/main/java/com/example/smartcity3dar/MainActivity.kt
+++ SmartCity3Dar/app/src/main/java/com/example/smartcity3dar/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.smartcity3dar
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -8,6 +10,8 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.smartcity3dar.databinding.ActivityMainBinding
+import com.example.smartcity3dar.databinding.ModalLoginBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +23,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val signInButton: Button = findViewById(R.id.signInButton)
+
+        signInButton.setOnClickListener {
+            val view: View = layoutInflater.inflate(R.layout.modal_login, null)
+            val modal = BottomSheetDialog(this)
+            modal.setContentView(view)
+            modal.show()
+        }
         /*val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
