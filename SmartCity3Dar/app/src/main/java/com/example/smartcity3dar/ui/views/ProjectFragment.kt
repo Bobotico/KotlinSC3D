3b2505aad5d7e011a780774af1fe09e9a738267e
Francesco Paciello <francescopaciello132@gmail.com>
2023-06-21 13:28:59 +0200

    MapBox + progetti inizio

--- SmartCity3Dar/app/src/main/java/com/example/smartcity3dar/ui/views/ProjectFragment.kt
+++ SmartCity3Dar/app/src/main/java/com/example/smartcity3dar/ui/views/ProjectFragment.kt
@@ -1,13 +1,19 @@
 package com.example.smartcity3dar.ui.views
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.smartcity3dar.R
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.smartcity3dar.databinding.AvailableProjectsBinding
+import com.example.smartcity3dar.databinding.ModalLoginBinding
+import com.example.smartcity3dar.ui.home.LoginViewModel
 import com.example.smartcity3dar.ui.viewModels.ProjectViewModel
+
 
 class ProjectFragment : Fragment() {
 
@@ -17,16 +23,23 @@ class ProjectFragment : Fragment() {
 
     private lateinit var viewModel: ProjectViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ProjectViewModel::class.java)
-    }
+    private var _binding: AvailableProjectsBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_project, container, false)
+        val loginViewModel =
+            ViewModelProvider(this).get(ProjectViewModel::class.java)
+
+        _binding = AvailableProjectsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        //binding.projectsScrollView
+
+        return root
     }
 
 }
